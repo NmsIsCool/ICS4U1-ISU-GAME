d9461fb48770361051b6c640d005a81ee0bcfbbe
Jack Sherwood <jackaroosherwood@gmail.com>
2025-06-14 00:01:56 -0400

    Reverting to old repo version

---
+++ src/goi.java
@@ -0,0 +1,30 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.TrueTypeFont;
+
+public class goi extends BasicGameState {
+
+    
+
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+
+   }
+          
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException { 
+
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+       
+    }
+        
+    public int getID() {
+      return 22; // this id will be different for each screen
+   }
+
+    }
+
